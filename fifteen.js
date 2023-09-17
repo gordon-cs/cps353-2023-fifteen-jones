c40b70a78e74e4ca3a80acb57ba8c47f82f0767b
matthew.jones <matthew.jones@gordon.edu>
2023-09-16 20:37:13 -0400

    Completed via pair programing

--- fifteen.js
+++ fifteen.js
@@ -1,6 +1,21 @@
-mountBackground();
+let tiles = [].slice.call(document.getElementsByTagName("div")).slice(1);
+console.log(tiles);
+let positions = ["one_one", "one_two", "one_three", "one_four", 
+                 "two_one", "two_two", "two_three", "two_four", 
+                 "three_one", "three_two", "three_three", "three_four",
+                 "four_one", "four_two", "four_three", "four_four"]
+
+setupPosition();
+// mountBackground();
+
+function setupPosition() {
+  for(let i = 0; i<tiles.length; i++) {
+    let tile = tiles[i];
+    tile.classList.add(positions[i]);
+  }
+}
 
 function mountBackground() {
-  let divs = document.getElementsByTagName("div");
+  
   console.log(divs);
 }
