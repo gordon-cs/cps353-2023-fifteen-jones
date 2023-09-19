38e9c5eda747da539706ca8c031689c10108a58d
Luke Hart <luke.hart@gordon.edu>
2023-09-18 20:24:21 -0400

    Fixed requirements (Pair programming)

--- fifteen.js
+++ fifteen.js
@@ -1,32 +1,36 @@
 "use strict";
 
 let tiles = [].slice.call(document.getElementsByTagName("div")).slice(1);
-console.log(tiles);
-let positions = ["one_one", "one_two", "one_three", "one_four", 
-                 "two_one", "two_two", "two_three", "two_four", 
-                 "three_one", "three_two", "three_three", "three_four",
-                 "four_one", "four_two", "four_three", "four_four"]
+let emptySquare =["300px","300px"];
 
-setupPosition();
-mountBackground();
+document.getElementById("puzzlearea").addEventListener('click', moveTiles);
 
-function setupPosition() {
-  for(let i = 0; i<tiles.length; i++) {
-    let tile = tiles[i];
-    tile.classList.add(positions[i]);
-    tile.style.backgroundImage = "url('background.jpg')";
-  }
-}
+setUpBoard();
 
-function mountBackground() {
+function setUpBoard() {
   for (let r = 0; r < 4; r++) {
     for (let c = 0; c < 4; c++) {
       let index = (r * 4) + c;
       if (index < 15) {
-        let yPos = -100 * r;
-        let xPos = -100 * c;
-        tiles[index].style.backgroundPosition = xPos + "px " + yPos + "px";
+        let xPos = 100 * c;
+        let yPos = 100 * r;
+        tiles[index].style.marginLeft = xPos + "px";
+        tiles[index].style.marginTop = yPos + "px";
+        tiles[index].style.backgroundImage = "url('background.jpg')";
+        tiles[index].style.backgroundPosition = -xPos + "px " + -yPos + "px";
       }
     }
   }
 }
+
+function moveTiles(event) {
+  let tile = tiles[event.target.innerText - 1];
+  let tilePosX = tile.style.marginLeft;
+  let tilePosY = tile.style.marginTop;
+ // if (Math.abs(tilePosX.substring(0,-2)-emptySquare[1].substring(0,-2)))
+  console.log(Math.abs(tilePosX.substring(0,-2)-emptySquare[1].substring(0,-2)));
+  tile.style.marginLeft = emptySquare[0];
+  tile.style.marginTop  = emptySquare[1];
+  emptySquare = [tilePosX, tilePosY];
+}
+
