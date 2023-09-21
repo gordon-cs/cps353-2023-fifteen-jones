9b017f30f17e4d64971ce651ff4570a98958f0de
Luke Hart <luke.hart@gordon.edu>
2023-09-20 20:48:51 -0400

    Added hover (pair programming)

--- fifteen.js
+++ fifteen.js
@@ -6,6 +6,7 @@ let emptySquare =["300px","300px"];
 document.getElementById("puzzlearea").addEventListener('click', moveTiles);
 
 setUpBoard();
+setupHover();
 
 function setUpBoard() {
   for (let r = 0; r < 4; r++) {
@@ -18,6 +19,7 @@ function setUpBoard() {
         tiles[index].style.marginTop = yPos + "px";
         tiles[index].style.backgroundImage = "url('background.jpg')";
         tiles[index].style.backgroundPosition = -xPos + "px " + -yPos + "px";
+        tiles[index].classList.add("standard");
       }
     }
   }
@@ -25,16 +27,35 @@ function setUpBoard() {
 
 function moveTiles(event) {
   let tile = tiles[event.target.innerText - 1];
+  if (isSlidable(event.target.innerText - 1)) {
+    let tilePosX = tile.style.marginLeft;
+    let tilePosY = tile.style.marginTop;
+    tile.style.marginLeft = emptySquare[0];
+    tile.style.marginTop  = emptySquare[1];
+    emptySquare = [tilePosX, tilePosY];
+  }
+  setupHover();
+}
+
+function setupHover() {
+  for(let i = 0; i < tiles.length; i++) {
+    let tile = tiles[i];
+    if (isSlidable(i)) {
+      tile.classList.add("hover");
+    } else {
+      tile.classList.remove("hover");
+    }
+  }
+}
+
+function isSlidable(index) {
+  let tile = tiles[index];
   let tilePosX = tile.style.marginLeft;
   let tilePosY = tile.style.marginTop;
   let xDistance = Math.abs(tilePosX.substring(0,tilePosX.length-2) -
                           emptySquare[0].substring(0,emptySquare[0].length-2));
   let yDistance = Math.abs(tilePosY.substring(0,tilePosY.length-2) -
                           emptySquare[1].substring(0,emptySquare[1].length-2));
-  if ((xDistance <= 100 && yDistance == 0) ||
-      (xDistance == 0 && yDistance <= 100)) {
-    tile.style.marginLeft = emptySquare[0];
-    tile.style.marginTop  = emptySquare[1];
-    emptySquare = [tilePosX, tilePosY];
-  }
+    return ((xDistance <= 100 && yDistance == 0) ||
+            (xDistance == 0 && yDistance <= 100));
 }
